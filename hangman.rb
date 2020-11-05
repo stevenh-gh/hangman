bc141894859950574bd462b42085406edecbe3d2
steven huynh <steven.h@live.com>
2020-11-04 22:44:47 -0800

    turned file into game class

--- hangman.rb
+++ hangman.rb
@@ -1,75 +1,160 @@
 require_relative 'sample'
 
+class Hangman
+  attr_accessor :dict, :sample, :amt_guesses, :word, :guessed_right, :guessed_wrong
+
+  def initialize
+    @dict = load_dictionary
+    @sample = Sample.new dict
+    @amt_guesses = 6 # Stick figure takes 6 marks to complete
+    @word = Array.new sample.random_word.length, '_'
+    @guessed_right = []
+    @guessed_wrong = []
+  end
+
+  def play
+    #     display_guess amt_guesses
+    #     display_word word
+    display_word_guess word, amt_guesses
+    separator
+    loop do
+      # Ask user to guess
+      guess = query_player(guessed_right + guessed_wrong)
+
+      if sample.random_word.downcase.include? guess
+        word.map!.with_index { |ele, idx| sample.random_word[idx].downcase == guess ? sample.random_word[idx] : ele }
+        guessed_right << guess
+      else
+        self.amt_guesses -= 1
+        guessed_wrong << guess
+      end
+
+      #       display_guess amt_guesses
+      #       display_word word
+      display_word_guess word, amt_guesses
+
+      print "Correctly guessed: #{guessed_right}\n"
+      print "Already guessed: #{guessed_wrong}\n"
+      #       puts '******************************************************************'
+      separator
+
+      if amt_guesses == 0
+        puts 'You have failed to guess the word!'
+        break
+      end
+
+      if word.join('') == sample.random_word
+        puts 'You have guessed the word correctly!'
+        break
+      end
+    end
+  end
+
+  private
+
+  def load_dictionary
+    File.readlines('5desk.txt').map { |row| row.chomp }
+  end
+
+  #   def display_word(word)
+  #     word.each { |char| print "#{char} " }
+  #     puts
+  #   end
+
+  #   def display_guess(amt_guesses)
+  #     puts "Amount of guesses: #{amt_guesses}"
+  #   end
+
+  def display_word_guess(word, amt_guesses)
+    puts "Amount of guesses: #{amt_guesses}"
+    word.each { |char| print "#{char} " }
+    puts
+  end
+
+  def separator
+    puts '******************************************************************'
+  end
+
+  def query_player(guessed_array)
+    valid_guess = false
+    until valid_guess
+      print 'Enter a character to guess: '
+      guess = gets.chomp.downcase
+      valid_guess = true if ('a'..'z').to_a.include?(guess) && !guessed_array.include?(guess)
+    end
+    guess
+  end
+end
+
 puts 'Hangman initialized!'
 
-def load_dictionary
-  File.readlines('5desk.txt').map { |row| row.chomp }
-end
+# def display_word(word)
+#   word.each { |char| print "#{char} " }
+#   puts
+# end
 
-def display_word(word)
-  word.each { |char| print "#{char} " }
-  puts
-end
+# def query_player(guessed_array)
+#   valid_guess = false
+#   until valid_guess
+#     print 'Enter a character to guess: '
+#     guess = gets.chomp.downcase
+#     valid_guess = true if ('a'..'z').to_a.include?(guess) && !guessed_array.include?(guess)
+#   end
+#   guess
+# end
 
-def query_player(guessed_array)
-  valid_guess = false
-  until valid_guess
-    print 'Enter a character to guess: '
-    guess = gets.chomp.downcase
-    valid_guess = true if ('a'..'z').to_a.include?(guess) && !guessed_array.include?(guess)
-  end
-  guess
-end
+# def display_guess(amt_guesses)
+#   puts "Amount of guesses: #{amt_guesses}"
+# end
 
-def display_guess(amt_guesses)
-  puts "Amount of guesses: #{amt_guesses}"
-end
-
-dict = load_dictionary
+# dict = load_dictionary
 # sample.random_word = generate_random_word dict
-sample = Sample.new dict
-p sample.random_word
+# sample = Sample.new dict
+# p sample.random_word
 
-amt_guesses = 6 # Stick figure takes 6 marks to complete
+# amt_guesses = 6 # Stick figure takes 6 marks to complete
 
-word = Array.new sample.random_word.length, '_'
+# word = Array.new sample.random_word.length, '_'
 
 # Display amount of guesses
-display_guess amt_guesses
+# display_guess amt_guesses
 
 # Display word
-display_word word
+# display_word word
 
-puts '******************************************************************'
+# puts '******************************************************************'
 
-guessed_right = []
-guessed_wrong = []
+# guessed_right = []
+# guessed_wrong = []
 
-loop do
-  # Ask user to guess
-  guess = query_player(guessed_right + guessed_wrong)
+# loop do
+#   # Ask user to guess
+#   guess = query_player(guessed_right + guessed_wrong)
 
-  if sample.random_word.downcase.include? guess
-    word.map!.with_index { |ele, idx| sample.random_word[idx].downcase == guess ? sample.random_word[idx] : ele }
-    guessed_right << guess
-  else
-    amt_guesses -= 1
-    guessed_wrong << guess
-  end
+#   if sample.random_word.downcase.include? guess
+#     word.map!.with_index { |ele, idx| sample.random_word[idx].downcase == guess ? sample.random_word[idx] : ele }
+#     guessed_right << guess
+#   else
+#     amt_guesses -= 1
+#     guessed_wrong << guess
+#   end
 
-  display_guess amt_guesses
-  display_word word
-  print "Correctly guessed: #{guessed_right}\n"
-  print "Already guessed: #{guessed_wrong}\n"
-  puts '******************************************************************'
+#   display_guess amt_guesses
+#   display_word word
+#   print "Correctly guessed: #{guessed_right}\n"
+#   print "Already guessed: #{guessed_wrong}\n"
+#   puts '******************************************************************'
 
-  if amt_guesses == 0
-    puts 'You have failed to guess the word!'
-    break
-  end
+#   if amt_guesses == 0
+#     puts 'You have failed to guess the word!'
+#     break
+#   end
 
-  if word.join('') == sample.random_word
-    puts 'You have guessed the word correctly!'
-    break
-  end
-end
+#   if word.join('') == sample.random_word
+#     puts 'You have guessed the word correctly!'
+#     break
+#   end
+# end
+
+game = Hangman.new
+game.play
